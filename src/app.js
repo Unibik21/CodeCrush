1690085cd1494ec5c5421714bd7206b3e90bc8fc
Unibik21 <utkarshcha21@gmail.com>
2026-02-28 13:19:16 +0530

    debugging

--- src/app.js
+++ src/app.js
@@ -1,39 +1,11 @@
-const express = require('express');
-const connectDB = require('./config/database');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
 const app = express();
 
-app.use(cors({
-    origin: "http://16.170.215.1:3000",
-    credentials:true,
-}));
-app.use(express.json());
-app.use(cookieParser());
-
-const authRouter = require('./routes/auth');
-const reqRouter = require('./routes/request');
-const profileRouter = require('./routes/profile');
-const userRouter = require('./routes/user')
-
 app.get("/", (req, res) => {
-    res.send("Backend working");
-});
-app.use("/",authRouter);
-app.use("/",reqRouter);
-app.use("/",profileRouter);
-app.use("/",userRouter);
-
-connectDB()
-.then(() => {
-    console.log("Server Connected Successfully!");
-    app.listen(3000,"0.0.0.0",()=>{
-        console.log("SERVER RUNNING ON PORT NUMBER 3000");
-    });
-})
-.catch((err) => {
-    console.error("Caught Error");
+  console.log("Root route hit");
+  res.send("WORKING PERFECTLY");
 });
 
-
-
+app.listen(3000, "0.0.0.0", () => {
+  console.log("TEST SERVER RUNNING");
+});
